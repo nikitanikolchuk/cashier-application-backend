3af17f0a8270ac6f7bc37071dfa0ce8aa4acbb8c
Nikita Nikolchuk <nikolnik@fit.cvut.cz>
2023-01-13 10:15:07 +0100

    Add OpenAPIDefinition

--- src/main/java/cz/cvut/fit/tjv/cashier_application/CashierApplication.java
+++ src/main/java/cz/cvut/fit/tjv/cashier_application/CashierApplication.java
@@ -1,8 +1,15 @@
 package cz.cvut.fit.tjv.cashier_application;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(title = "Cashier Application", version = "1.0.0", description = "Application for saving orders."),
+		servers = @Server(description = "local server", url = "http://localhost:8080")
+)
 @SpringBootApplication
 public class CashierApplication {
 
